a710fa0267ba6e657043559bfdb2c63bf918ebad
Diego Santos <Diegoalbertosantos91@gmail.com>
2022-06-08 14:55:17 -0300

    Seccion 8

--- database/config.js
+++ database/config.js
@@ -2,11 +2,25 @@ const mongoose = require('mongoose');
 
 
 
-const dbConnection = async()=>{};
+const dbConnection = async()=>{
+
+try {
+
+    mongoose.connect(process.env.MONGODB_CNN);
+    
+    console.log('base conectada');
+    
+    
+} catch (error) {
+    console.log(error);
+    throw new Error('Error a la hora de iniciar la base de datos');
+}
+
+};
 
 
 
 
 module.exports={
     dbConnection
-}
+};
--- models/server.js
+++ models/server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 var cors = require('cors')
-
+const { dbConnection } = require('../database/config');
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
+        //connect sb
+        this.conectarDB();
         //middlewares
         this.middlewares();
         //rutas de mi app
         this.routes();
     }
-
+    async conectarDB() {
+        await dbConnection();
+    }
     middlewares() {
         //cors
         this.app.use(cors())
